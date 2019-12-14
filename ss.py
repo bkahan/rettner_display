19c6af3cd7c1b2063137e40e9d4f1337a1bd6e38
Ben Kahan <ben@aeronx.com>
2019-12-14 16:59:07 -0500

    updated location, updated for python3

--- ss.py
+++ ss.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
 """
-A pygame program to show a slideshow of all images buried in a given directory.
+original code: https://github.com/andrewdelph/python-slideshow-with-time-and-weather
 
-Originally Released: 2007.10.31 (Happy halloween!)
+updated for rettner media lab, author: ben kahan 2019
 
 """
 from __future__ import division
@@ -13,19 +12,14 @@ import sys
 import time
 import datetime
 
-
 import pygame
 from pygame.locals import QUIT, KEYDOWN, K_ESCAPE
 
-
-#weather
 import pyowm
 
-
-
 file_list = []  # a list of all images being shown
-title = "pgSlideShow | My Slideshow!"  # caption of the window...
-waittime = 5   # default time to wait between images (in seconds)
+title = "Rettner Media Lab"  # caption of the window...
+waittime = 5  # default time to wait between images (in seconds)
 
 
 def walktree(top, callback):
@@ -44,143 +38,144 @@ def walktree(top, callback):
             callback(pathname)
         else:
             # Unknown file type, print a message
-            print 'Skipping %s' % pathname
+            print('Skipping %s' % pathname)
 
 
-def addtolist(file, extensions=['.png', '.jpg', '.jpeg', '.gif', '.bmp']):
+def addtolist(file, extensions=None):
     """Add a file to a global list of image files."""
+    if extensions is None:
+        extensions = ['.png', '.jpg', '.jpeg', '.gif', '.bmp']
     global file_list  # ugh
     filename, ext = os.path.splitext(file)
     e = ext.lower()
     # Only add common image types to the list.
     if e in extensions:
-        print 'Adding to list: ', file
+        print('Adding to list: ', file)
         file_list.append(file)
     else:
-        print 'Skipping: ', file, ' (NOT a supported image)'
+        print('Skipping: ', file, ' (NOT a supported image)')
 
 
 def input(events):
     """A function to handle keyboard/mouse/device input events. """
     for event in events:  # Hit the ESC key to quit the slideshow.
         if (event.type == QUIT or
-            (event.type == KEYDOWN and event.key == K_ESCAPE)):
+                (event.type == KEYDOWN and event.key == K_ESCAPE)):
             pygame.quit()
 
 
-def timeSince(lastTime,interval):
-    if (time.time() - lastTime)>=interval:
+def timeSince(lastTime, interval):
+    if (time.time() - lastTime) >= interval:
         return True
     else:
         return False
-    
-    
+
+
 def main(startdir="."):
     global file_list, title, waittime
-    lastSwitch=time.time()
-    lastWeather=time.time()
-    
-    owm = pyowm.OWM('4cc9ae1d116c7e70c145252ab605f260')
-    observation = owm.weather_at_place('Ottawa,CA')
-    w = observation.get_weather()
-    temperature=(w.get_temperature('celsius'))['temp']
-    status=w.get_status()
-    #print w
+    lastSwitch = time.time()
+    lastWeather = time.time()
 
+    location = 'Rochester,NY,USA'
+    tempUnit = 'fahrenheit'
+
+    owm = pyowm.OWM('98b2dc257732f2825cf3a14eaff380bd')
+    observation = owm.weather_at_place(location)
+    w = observation.get_weather()
+    temperature = (w.get_temperature(tempUnit))['temp']
+    status = w.get_status()
     pygame.init()
 
     # Test for image support
     if not pygame.image.get_extended():
-        print "Your Pygame isn't built with extended image support."
-        print "It's likely this isn't going to work."
+        print("Your Pygame isn't built with extended image support.")
+        print("It's likely this isn't going to work.")
         sys.exit(1)
 
     walktree(startdir, addtolist)  # this may take a while...
     if len(file_list) == 0:
-        print "Sorry. No images found. Exiting."
+        print("Sorry. No images found. Exiting.")
         sys.exit(1)
 
     modes = pygame.display.list_modes()
-    print max(modes)
+    print(max(modes))
     pygame.display.set_mode(max(modes))
 
     screen = pygame.display.get_surface()
-    screen_width, screen_height= screen.get_size()
+    screen_width, screen_height = screen.get_size()
     pygame.display.set_caption(title)
-    #pygame.display.toggle_fullscreen()
-    pygame.display.set_mode(max(modes),pygame.FULLSCREEN)
-    
+    pygame.display.set_mode(max(modes), pygame.FULLSCREEN)
+
     pygame.mouse.set_visible(0)
-    
-    #create font
+
+    # create font
     timeFont = pygame.font.Font("indulta/Indulta-SemiSerif-boldFFP.otf", 100)
     dateFont = pygame.font.Font("indulta/Indulta-SemiSerif-boldFFP.otf", 60)
     weatherFont = pygame.font.Font("indulta/Indulta-SemiSerif-boldFFP.otf", 60)
-    
-    print str(waittime) +"wait time"
-    
+
+    print(str(waittime) + "wait time")
 
     current = 0
     num_files = len(file_list)
-    while(True):
+
+    while True:
         try:
             img = pygame.image.load(file_list[current])
             img = img.convert()
-            tempX,tempY=img.get_size()
-            ratio =tempX/tempY
-            tempSize=(screen_width,int(screen_width/ratio))
-            print str(img.get_size())+" to "+ str(tempSize) +"and ratio: "+str(ratio)
+            tempX, tempY = img.get_size()
+            ratio = tempX / tempY
+            tempSize = (screen_width, int(screen_width / ratio))
+            print(str(img.get_size()) + " to " + str(tempSize) + "and ratio: " + str(ratio))
             # rescale the image to fit the current display
             img = pygame.transform.scale(img, tempSize)
             screen.blit(img, (0, 0))
-            
-            #gets current weather
-            if timeSince(lastWeather,30):
-                observation = owm.weather_at_place('Ottawa,CA')
+
+            # gets current weather
+            if timeSince(lastWeather, 30):
+                observation = owm.weather_at_place(location)
                 w = observation.get_weather()
-                temperature=(w.get_temperature('celsius'))['temp']
-                status=w.get_status()
-                print status
-                lastWeather=time.time()
-                print "updateing weather"
-            
-            #gets the current time and displays it
-            timeText=datetime.datetime.now().strftime("%I:%M%p")
-            dateText=datetime.datetime.now().strftime("%B %d, %Y")
-            weatherText=str(int(temperature))+"`C  "+status
-            
-            
-            timeLabel = timeFont.render(timeText, 1, (255,255,255))
-            dateLabel = dateFont.render(dateText, 1, (255,255,255))
-            weatherLabel = weatherFont.render(weatherText, 1, (255,255,255))
-            
-            timeWidth, timeHeight= timeLabel.get_size()
-            dateWidth, dateHeight= dateLabel.get_size()
-            weatherWidth, weatherHeight= weatherLabel.get_size()
-            
-            screen.blit(weatherLabel, (0, screen_height-weatherHeight))
-            
-            screen.blit(timeLabel, (screen_width-timeWidth, screen_height-timeHeight-dateHeight))
-            screen.blit(dateLabel, (screen_width-dateWidth, screen_height-dateHeight))
-            
+                temperature = (w.get_temperature(tempUnit))['temp']
+                status = w.get_status()
+                print(status)
+                lastWeather = time.time()
+                print("updating weather")
+
+            # gets the current time and displays it
+            timeText = datetime.datetime.now().strftime("%I:%M%p")
+            dateText = datetime.datetime.now().strftime("%B %d, %Y")
+            weatherText = str(int(temperature)) + "°F  " + status
+
+            timeLabel = timeFont.render(timeText, 1, (255, 255, 255))
+            dateLabel = dateFont.render(dateText, 1, (255, 255, 255))
+            weatherLabel = weatherFont.render(weatherText, 1, (255, 255, 255))
+
+            timeWidth, timeHeight = timeLabel.get_size()
+            dateWidth, dateHeight = dateLabel.get_size()
+            weatherWidth, weatherHeight = weatherLabel.get_size()
+
+            screen.blit(weatherLabel, (0, screen_height - weatherHeight))
+
+            screen.blit(timeLabel, (screen_width - timeWidth, screen_height - timeHeight - dateHeight))
+            screen.blit(dateLabel, (screen_width - dateWidth, screen_height - dateHeight))
+
             pygame.display.flip()
 
             input(pygame.event.get())
-            time.sleep(1/60)
+            time.sleep(1 / 60)
         except pygame.error as err:
-            print "Failed to display %s: %s" % (file_list[current], err)
+            print("Failed to display %s: %s" % (file_list[current], err))
             sys.exit(1)
 
         # When we get to the end, re-start at the beginning
-        if timeSince(lastSwitch,waittime):
+        if timeSince(lastSwitch, waittime):
             current = (current + 1) % num_files;
-            lastSwitch=time.time()
+            lastSwitch = time.time()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Recursively loads images '
-        'from a directory, then displays them in a Slidshow.'
+                    'from a directory, then displays them in a Slidshow.'
     )
 
     parser.add_argument(
@@ -208,7 +203,7 @@ if __name__ == '__main__':
         help='Set the title for the display window.'
     )
     args = parser.parse_args()
-    #waittime = args.waittime
-    
+    # waittime = args.waittime
+
     title = args.title
     main(startdir=args.path)
