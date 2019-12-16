e9c88babc33d1d9e8e68957676c07f3ef6a311a8
Ben Kahan <ben@aeronx.com>
2019-12-15 19:48:58 -0500

    added authentication checking

--- googleDrive.py
+++ googleDrive.py
@@ -2,19 +2,24 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
 
+
 foldertofind = 'Rettner Files'
 folderfound = False
-filestodownload = []
-
 gAuth = GoogleAuth()
+drive = GoogleDrive(gAuth)
 
 
 def authenticateUser():
-    gAuth.LoadCredentialsFile("credentials.txt")
+    gAuth.LoadCredentialsFile("credentials.txt")  # checks and loads a file holding the credentials of the user,
+    # still needs the client_secrests.json file to authenticate with google drive API
     if gAuth.credentials is None:
-        gAuth.LocalWebserverAuth()
+        gAuth.LocalWebserverAuth()  # open a local webserver to authenticate, only needs to be loaded once
     elif gAuth.access_token_expired:
-        gAuth.Refresh()
+        try:
+            gAuth.Refresh()
+        except:
+            print("Offline. Need to reauthenticate")
+            gAuth.LocalWebserverAuth()
     else:
         gAuth.Authorize()
     gAuth.SaveCredentialsFile("credentials.txt")
@@ -31,21 +36,18 @@ def getFileList():
             print('There is no folder called ' + foldertofind + '. Try again.')
     if folderfound is True:
         file_list = drive.ListFile({'q': " '1t0HENJysR4JbM2EtMl-4--NrF4JC0kVK' in parents and trashed=false"}).GetList()
-        return file_list
+        return file_list  # returns a list of files to be downloaded
 
 
 def downloadFiles(file_list=None):
-    path = './pictures'
+    path = './pictures'  # download files to the path the slideshow looks in
     os.chdir(path)
     for f in file_list:
         fileName = f['title']
-        print('downloading {}'.format(fileName))
+        print('Downloading {}'.format(fileName))
         f_ = drive.CreateFile({'id': f['id']})
         f_.GetContentFile(fileName)
-    os.chdir('..')
-
-
-drive = GoogleDrive(gAuth)
+    os.chdir('..')  # return to the tld
 
 
 def main():
